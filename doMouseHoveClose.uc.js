3c02c49c8b8f327c79aa3fb0ff1a8e961aa9ba46
shyangs <shyangs@gmail.com>
2013-11-17 17:20:36 +0800

    test

---
+++ doMouseHoveClose.uc.js
@@ -0,0 +1,40 @@
+// 不用點擊，而是用懸停關閉按鈕的方式來關閉分頁/標籤
+// close tab by hovering over close button
+(function(){
+
+let delay = 500 ;//懸停幾毫秒，才執行動作
+
+let ttb = document.getElementById('TabsToolbar');
+if(!ttb)return;
+let searchTabParent = function(node){
+	let node = node.parentElement;
+	if(null===node){
+		return null;
+	}else if('tab'===node.nodeName){
+		return node;
+	}else{
+		return searchTabParent(node);
+	}
+};
+let listener = function(event){
+	let target = event.originalTarget,
+		className = target.getAttribute('class');
+	if(className.indexOf('tab-close-button')!== -1 || className.indexOf('tabs-closebutton')!== -1){
+		//ttb.removeEventListener('mouseover', listener, false);
+		let timeoutID = setTimeout(function(){
+			let tab = searchTabParent(target);
+			tab?gBrowser.removeTab(tab):BrowserCloseTabOrWindow();
+			//fn();
+		}, delay);
+		target.addEventListener('mouseout', function(){
+			(typeof(timeoutID)!=='undefined')&&clearTimeout(timeoutID);
+			//fn();
+		}, false);
+	}
+};
+let fn = function(){
+	ttb.addEventListener('mouseover', listener, false);
+};
+fn();
+
+})();
